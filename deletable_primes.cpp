fae9ef97469b5baafa59d86dbc10fdfd337c7641
NathalieHerzog <nathalieherzog2003@gmail.com>
2019-02-25 20:07:54 +0100

    works (on paper)

--- deletable_primes.cpp
+++ deletable_primes.cpp
@@ -10,3 +10,98 @@
  * Implementation of deletable_primes.h.
  * ----------------------------------------------------------
  */
+
+#include "deletable_primes.h"
+
+int ways;
+
+unsigned long remove_digit(int index, unsigned long n)
+{
+    int count = 0;
+    long temps = n;
+
+    //determine the length of n
+    while (temps >= 1)
+    {
+        temps /= 10;
+        count++;
+    }
+
+    int temp[count];
+    int tempTemp[count - 1];
+
+    //save digits of n into the array
+    temps = n;
+    for (int i = count - 1; i >= 0; i--)
+    {
+        temp[i] = temps % 10;
+        temps /= 10;
+    }
+
+    //save the elements of the first array in a new one - without the one to be removed
+    int u = 0;
+    int j = 0;
+    while (u < count)
+    {
+        if (u != (index + (count - index - 1)))
+        {
+            tempTemp[j] = temp[u];
+            j++;
+        }
+
+        u++;
+    }
+
+    unsigned long newLong;
+
+    //convert array back to unsigned long
+    for (int i = 0; i < count; i++)
+    {
+        newLong = 10 * newLong + tempTemp[i];
+    }
+
+    return newLong;
+}
+
+int get_ways(unsigned long n)
+{
+    if (n < 10 && is_prime(n))
+    {
+        return 1;
+    }
+
+    int ways = 0;
+    int count;
+    int temp = n;
+
+    while (temp >= 1)
+    {
+        temp /= 10;
+        count++;
+    }
+
+    for (int i = 0; i < count; i++)
+    {
+        if (is_prime(remove_digit(n, i)))
+        {
+            ways += get_ways(remove_digit(i, n));
+        }
+    }
+
+    return ways;
+}
+
+bool is_prime(unsigned long n)
+{
+    int temp = n;
+
+    for (int i = 2; i < temp; i++)
+    {
+        if (i != temp && temp % i == 0)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
